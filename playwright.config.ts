05e2bd0ea197b4ca08962e935d61924b1ad9bdc1
RUSTEMATOR <samoilenkorustem@gmail.com>
2025-12-23 13:16:39 +0200

    a

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,7 +28,7 @@ export default defineConfig({
     trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    baseURL: process.env.BASE_URL || 'https://kingbillycasino.com',
+    baseURL: process.env.BASE_URL || 'https://www.kingbillycasino.com',
   },
 
   /* Configure projects for major browsers */
